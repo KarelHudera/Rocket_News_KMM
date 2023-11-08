0279596f64fad3598f119688c1268502c807e44f
Karel Hudera <karel.hudera@gmail.com>
2023-11-08 21:00:43 +0100

    NASA api issues

--- app/src/commonMain/kotlin/com/example/rocketnews/data_remote/model/apiNews/ApiNews.kt
+++ app/src/commonMain/kotlin/com/example/rocketnews/data_remote/model/apiNews/ApiNews.kt
@@ -4,11 +4,12 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ApiNews(
+//    val copyright: String, // TODO: sometimes response is without this parameter
     val date: String,
     val explanation: String,
-    val hdurl: String,
+    val hdurl: String, // TODO: sometimes response is without this parameter
     val media_type: String,
     val service_version: String,
     val title: String,
-    val url: String
+    val url: String // TODO: sometimes response is YouTube url
 )
